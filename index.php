739990e0b9e960d27f4a1d23febd5e5e2a433470
Ugo DA SILVA <ugodasilva67@gmail.com>
2022-10-17 14:01:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
 // Souvent on identifie cet objet par la variable $conn ou $db
 $conn = new PDO(
-    'mysql:host=localhost;dbname=cinema;charset=utf8',
-    'root',
-    'root'
+    'mysql:host = localhost ; dbname = cinema ; charset = utf8', // mysql -> DSN(data source name)
+    'root', // login
+    'root'  //mot de passe
 );
 
 ?>
