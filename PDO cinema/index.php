128a58d11123d8da77f78319303cf9725d97c00d
Ugo DA SILVA <ugodasilva67@gmail.com>
2022-10-17 15:31:01 +0200

    save

--- PDO cinema/index.php
+++ PDO cinema/index.php
@@ -1,29 +1,22 @@
 <?php
 
+// creation login
+$user = 'root';
+$pass ='';
+
 
 // AFFICHAGE ERREUR
 // https://openclassrooms.com/fr/courses/1665806-programmez-en-oriente-objet-en-php/7307123-gerez-vos-cas-derreur#r-7307118
 try {
-    // Souvent on identifie cet objet par la variable $conn ou $db
-$conn = new PDO(
-    'mysql:host = localhost ; dbname = cinema ; charset = utf8', // mysql -> DSN(data source name)
-    'root', // login
-    ''  //mot de passe
-);
-} catch (Exception $e) {
-    die('Erreur : ' . $e->getMessage());
+
+  $db = new PDO('mysql:host=localhost;dbname=cinema;charset = utf8', $user, $pass);// mysql -> DSN(data source name)
+  
+  foreach ($db->query('SELECT * FROM personne') as $row) {
+    print_r($row);
+  }
+} catch (PDOException $e) {
+  
+  die('Erreur : '.$e->getMessage().'<br>');
 }
 
-//recupère le contenu de la table personne
-$sqlQuery = 'SELECT * FROM personne';
-$personneStatement = $conn->prepare($sqlQuery);
-$personneStatement->execute();
-$personne = $personneStatement->fetchAll(); // fetch -> va chercher
-
-// affichage de la table une par une
-foreach ($personne as $personnes) {
-?>
-    <p><?php echo $personne['nom'.'prenom']; ?></p>
-<?php
-}
 ?>
