3b12eea24e5d32220a0ac9eb5bb893f540b82a2f
Ugo DA SILVA <ugodasilva67@gmail.com>
2022-10-17 11:27:03 +0200

    save

--- HeidiSQL/requetes/L.sql
+++ HeidiSQL/requetes/L.sql
@@ -1,1 +1,7 @@
 SELECT CONCAT(p.prenom,' ',p.nom) AS nomActeur
+FROM personne p 
+INNER JOIN acteur a ON p.id_personne = a.id_personne
+INNER JOIN jouer j ON j.id_acteur = a.id_acteur
+GROUP BY nomActeur
+HAVING COUNT(j.id_acteur)>= 3
+
